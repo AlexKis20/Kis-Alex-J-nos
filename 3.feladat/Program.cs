452f5c0b17855bd4d7426e4e0524eb424dc1696d
kisrondox5@gmail.com <kisrondox5@gmail.com>
2025-02-10 14:10:12 +0100

    3.feladat

---
+++ 3.feladat/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3.feladat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kérlek add meg a nevedet: ");
+            string nev =Console.ReadLine();
+            Console.WriteLine("Kérlek add meg az életkorodat: ");
+            int kor = Convert.ToInt32(Console.ReadLine());
+            if (kor>=18)
+            {
+                Console.WriteLine($"{nev} nagykorú.");
+            }
+            else
+            {
+                Console.WriteLine($"{nev} kiskorú.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
