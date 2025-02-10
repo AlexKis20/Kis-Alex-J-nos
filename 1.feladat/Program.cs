1a4eb3213bc825e12776fa670396ebf16379a520
kisrondox5@gmail.com <kisrondox5@gmail.com>
2025-02-10 13:51:42 +0100

    Add project files.

---
+++ 1.feladat/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.feladat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Egy kg alma ára: ");
+            int egykgalma =Convert.ToInt16(Console.ReadLine());
+            Console.Write("Hány kg almát veszel: ");
+            double mennyikgkell = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine($"Ennyi alma ára {mennyikgkell * egykgalma} korona.");
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
